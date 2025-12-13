81837abd518f7408cd1d50a33a42bc4094bba5f6
Arief <ariefhussainmail@gmail.com>
2025-12-13 15:05:41 +0530

    scokets error

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app)
 
 const io = new Server(server,{
     cors:{
-        origin:["https://fullstack-chat-app-2ird.vercel.app"],
+        origin:["http://localhost:5173", "https://fullstack-chat-app-2ird.vercel.app"],
         credentials: true
     }
 });
